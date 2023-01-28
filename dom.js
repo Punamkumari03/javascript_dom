02648d4cf52ba3f4a5ea563374c2082bcddeb0c9
Punamkumari03 <punamgupta250602@gmail.com>
2023-01-28 18:44:52 +0530

    final commit

---
+++ dom.js
@@ -0,0 +1,22 @@
+// console.dir(document);
+// examine document object
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.all[10]);
+// // document.all[10].textContent='Hello';
+// console.log(document.forms);
+// console.log(document.getElementById('header-title'));
+var headerTitle=document.getElementById('header-title');
+var header=document.getElementById('main-header');
+var cardTitle=document.getElementById('item');
+// console.log(headerTitle);
+// headerTitle.textContent='Hello';
+header.style.borderBottom='solid 3px #000';
+cardTitle.style.color='green'
+cardTitle.style.fontweight='bold';
+
+
+// console.log(cardTitle);
