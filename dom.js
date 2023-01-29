aa94e1c4de070c134e1dcb0e5ebe41baea2e0b70
Punamkumari03 <punamgupta250602@gmail.com>
2023-01-29 18:52:50 +0530

    getelementbytagname added

--- dom.js
+++ dom.js
@@ -37,3 +37,17 @@ items[2].style.backgroundColor='green';
 for(var i=0; i<items.length;i++){
     items[i].style.fontweight='bold';
 }
+//getelementbytagname
+var li=document.getElementsByTagName('li');
+console.log(li)
+console.log(li[1]);
+li[1].textContent='Hello 3';
+li[1].style.fontweight='bold';
+li[1].style.backgroundColor='yellow';
+for(var i=0; i<li.length;i++){
+    li[i].style.backgroundColor='#f4f4f4'
+}
+
+//queryselector
+var header=document.querySelector('#main-header');
+header.style.borderBottom='solid 4px #ccc'
