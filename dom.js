8371ab9c9c5f73a50037b1f8d3181ee782c81558
Punamkumari03 <punamgupta250602@gmail.com>
2023-01-29 19:21:58 +0530

    final commit

--- dom.js
+++ dom.js
@@ -51,3 +51,24 @@ for(var i=0; i<li.length;i++){
 //queryselector
 var header=document.querySelector('#main-header');
 header.style.borderBottom='solid 4px #ccc'
+var input = document.querySelector('input');
+input.value='Hell world'
+var submit = document.querySelector('input[type="submit"]');
+submit.value='SEND';
+var item = document.querySelector('.list-group-item');
+item.style.color='red'
+var lastItem = document.querySelector('.list-group-item:last-child');
+lastItem.style.color='blue'
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.color='orange'
+
+//queryselectrall
+var titles=document.querySelectorAll('.title')
+console.log(titles)
+titles[0].textContent='hello'
+var odd=document.querySelectorAll('li:nth-child(odd)');
+var even=document.querySelectorAll('li:nth-child(even)');
+for(var i=0;i<odd.length;i++){
+   odd[i].style.backgroundColor='green';
+   even[i].style.backgroundColor='#f4f6';
+}
