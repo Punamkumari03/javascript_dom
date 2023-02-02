6d48e5ff93c03055db4da860e4022454248b3e8b
Punamkumari03 <punamgupta250602@gmail.com>
2023-02-02 23:51:08 +0530

    description added

--- dom.js
+++ dom.js
@@ -149,29 +149,41 @@ function addItem(e){
 
   // Get input value
   var newItem = document.getElementById('item').value;
+  var desItem = document.getElementById('description').value;
+ 
 
   // Create new li element
   var li = document.createElement('li');
   // Add class
   li.className = 'list-group-item';
   // Add text node with input value
-  li.appendChild(document.createTextNode(newItem));
+  li.appendChild(document.createTextNode(newItem+''+desItem));
+  // li.appendChild(document.createTextNode(desItem));
+
+
 
   // Create del button element
   var deleteBtn = document.createElement('button');
+  // Create edit button element
+  var editBtn = document.createElement('button');
 
   // Add classes to del button
   deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+   // Add classes to edit button
+   editBtn.className = 'btn btn-success btn-sm float-right delete';
 
   // Append text node
   deleteBtn.appendChild(document.createTextNode('X'));
+  editBtn.appendChild(document.createTextNode('Edit'));
 
   // Append button to li
   li.appendChild(deleteBtn);
+  li.appendChild(editBtn);
 
   // Append li to list
   itemList.appendChild(li);
 }
+
 
 // Remove item
 function removeItem(e){
